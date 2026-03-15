558136fe2634b56a586874e6776d5948c9413fed
Jack <jack.d.center@gmail.com>
2026-03-15 15:25:00 -0600

    Add Initial Framework

---
+++ include/statefox.h
@@ -0,0 +1,9 @@
+#ifndef STATEFOX_H
+#define STATEFOX_H
+
+// Core FSM API
+void sf_init(void);
+
+void sf_dispatch(void);
+
+#endif // STATEFOX_H
---
+++ tests/test_statefox.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "statefox.h"
+
+void test_fsm_simple() {}
+
+int main() {
+    test_fsm_simple();
+    printf("No tests implemented\n");
+    return 0;
+}
